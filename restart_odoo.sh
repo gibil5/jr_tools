3e23f4eb61d0ec089c4f54f2d8c086cb58ae20ac
gibil <jrevilla55@gmail.com>
2018-04-23 11:29:21 -0500

    Initialize repository

---
+++ restart_odoo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LOG=/root/jr/restart.log
+
+SERVICE=odoo.py
+
+FULL=/usr/bin/odoo.py --config=/etc/odoo/openerp-server.conf
+
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+	echo "1"
+	echo "$SERVICE is running well at `date`" >> $LOG 
+else
+	echo "2"
+	echo "$SERVICE is not running. Restarting... at `date`" >> $LOG 
+
+        systemctl restart odoo
+fi
