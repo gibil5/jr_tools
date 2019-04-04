de411af6fd0c96e4b096be961fadde061fbeef6c
root <jrevilla55@gmail.com>
2019-04-04 19:07:32 +0000

    ..

--- restart_odoo.sh
+++ restart_odoo.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+# Odoo and IP Monitor
+# 4 Apr 2019 
 
+# Global vars
 LOG=/root/jr_tools/restart.log
-
 SERVICE=odoo.py
-
 #FULL=/usr/bin/odoo.py --config=/etc/odoo/openerp-server.conf
-
-
 
 # Odoo service
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
@@ -16,10 +15,8 @@ then
 else
 	echo "2"
 	echo "$SERVICE is not running. Restarting... at `date`" >> $LOG 
-
         systemctl restart odoo
 fi
-
 
 # IP Address
 #PS1=$(ifconfig $(route -n | grep ^0.0.0.0 | awk '{print $NF}') | grep inet | grep -v inet6 | awk '{print $2}')
@@ -27,7 +24,4 @@ PS1=$(/usr/sbin/ifconfig $(route -n | grep ^0.0.0.0 | awk '{print $NF}') | grep 
 echo "3"
 echo "$PS1" >> $LOG 
 echo "" >> $LOG 
-
-
-
 
